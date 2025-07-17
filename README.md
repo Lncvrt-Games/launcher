04db244c9794eadd60c7a1c9277be4c3aec0c4aa
Lncvrt <lncvrtreal@gmail.com>
2025-07-17 13:52:11 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Berry Dash Manager
+
+The version manager & launcher for Berry Dash users
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <main className="container">
+      <p>Berry Dash Manager</p>
+    </main>
+  );
+}
+
+export default App;
