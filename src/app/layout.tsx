5d025bb3e9d4942374b8815817e73e42cff86421
Lncvrt <lncvrtreal@gmail.com>
2025-10-03 18:48:18 -0700

    Revert "Make windows and macos have the same window style"
    This reverts commit 6c21759705f5ede0a03e426075d2b0caaf3965cd.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,6 @@ import axios from 'axios'
 import { openUrl } from '@tauri-apps/plugin-opener'
 import { GlobalProvider } from './GlobalProvider'
 import { Roboto } from 'next/font/google'
-import { getCurrentWindow } from '@tauri-apps/api/window'
 
 const roboto = Roboto({
   subsets: ['latin']
@@ -342,29 +341,9 @@ export default function RootLayout ({
                   <Sidebar />
                 </GlobalProvider>
                 <div
-                  className='dragarea'
-                  style={{
-                    height: '32px',
-                    width: '100vw',
-                    top: 0,
-                    left: 0,
-                    marginBottom: '-15px',
-                    position: 'absolute',
-                    zIndex: 9999,
-                    display: platform() == 'macos' ? 'block' : 'none',
-                    pointerEvents: 'auto'
-                  }}
-                  onMouseDown={() => {
-                    getCurrentWindow().startDragging()
-                  }}
-                ></div>
-                <div
                   className='relative z-[2] ml-[239px] w-[761px] border-b border-[#242424] h-[33px] bg-[#161616]'
                   style={{
-                    display:
-                      platform() == 'windows' || platform() == 'macos'
-                        ? 'block'
-                        : 'none'
+                    display: platform() == 'windows' ? 'block' : 'none'
                   }}
                 ></div>
                 <div className='relative z-0'>
